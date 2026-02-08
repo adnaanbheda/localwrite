c2cb049a8d1e3794bc9ad7d1a97b327d22254f45
Adnaan Bheda <adnaan.bheda@gmail.com>
2026-02-09 03:09:48 +0530

    reporters default

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,8 @@ export default defineConfig({
         include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
         alias: {
             '@': resolve(__dirname, './src')
-        }
+        },
+        reporters: ["default"]
     },
     resolve: {
         alias: {
